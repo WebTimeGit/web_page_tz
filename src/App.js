9813c5157f2462772e24102da0e0a81b3ece47a9
Web Time <>
2021-01-28 12:24:24 +0200

    creat components and css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
+import {BrowserRouter, Route} from "react-router-dom";
 import Login from "./components/Login/Login";
+import Registration from "./components/Registration/Registration";
+import Content from "./components/Content/Content";
 
 import './App.css';
 
 
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        react
-      </header>
-      <Login />
-    </div>
-  );
+   return (
+      <BrowserRouter>
+         <div className="App">
+            <header className="App-header">
+               react web page
+            </header>
+           <Route exact path='/' component={Login} />
+           <Route path='/registration' component={Registration} />
+            <Content/>
+
+         </div>
+      </BrowserRouter>
+   );
 }
 
 export default App;
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,7 +4,20 @@ import modClass from './Login.module.css';
 
 const Login = () => {
    return (
-      <p className={modClass.text}>login</p>
+      <form action="#" className={modClass.enter}>
+         <h2 className={modClass.titleForm}>Вход</h2>
+         <p>
+            <input type="text" placeholder="Логин" className={modClass.input}/>
+         </p>
+         <p>
+            <input type="password" placeholder="ввести пароль" className={modClass.input}/>
+         </p>
+
+         <button type="submit" className={modClass.pageBtn}>
+            войти
+         </button>
+
+      </form>
    )
 }
 
