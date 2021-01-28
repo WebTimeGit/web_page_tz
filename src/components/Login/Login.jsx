4f896c9fd71b4b64f5e365ab3bd45f5ec81e32a8
Web Time <>
2021-01-28 10:52:35 +0200

    creat web_page_tz

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import modClass from './Login.module.css';
+
+
+const Login = () => {
+   return (
+      <p className={modClass.text}>login</p>
+   )
+}
+
+export default Login;
