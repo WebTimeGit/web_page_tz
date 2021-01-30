e491d7812146677c5808a2588889129b4c3c33b8
Web Time <>
2021-01-30 21:01:46 +0200

    add component

---
+++ src/components/Content/ChangeParam/ChangeParamForm/ChangeParamForm.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+
+import modClass from "./ChangeParamForm.module.css";
+
+const ChangeParamForm = () => {
+   return (
+      <form action="#" className={modClass.registration}>
+         <p>
+            <input type="text" placeholder="ФИО" className={modClass.input}/>
+         </p>
+         <p>
+            <input type="text" placeholder="Должность" className={modClass.input}/>
+         </p>
+         <p>
+            <input type="tel" placeholder="Телефон" className={modClass.input}/>
+         </p>
+         <p>
+            <input type="text" placeholder="Логин" className={modClass.input}/>
+         </p>
+         <p>
+            <input type="password" placeholder="Пароль" className={modClass.input}/>
+         </p>
+         <p>
+            <input type="password" placeholder="Подтверждение пароля" className={modClass.input}/>
+         </p>
+
+         <div className={modClass.btnWr}>
+            <button type="submit" className={modClass.pageBtn}>
+               Сохранить
+            </button>
+            <NavLink exact to='/' className={modClass.pageBtn}>
+               назад
+            </NavLink>
+         </div>
+      </form>
+   )
+}
+
+export default ChangeParamForm;
